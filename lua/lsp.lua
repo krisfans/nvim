451f45fb9861e5e6f236b0eaebb2c8b303e39fc4
krisfans <krisfan_zhang@foxmail.com>
2021-12-10 12:30:22 +0800

    适配更新后的配置

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,84 +1,49 @@
 
-require'lspconfig'.vimls.setup{}
 require'lspconfig'.texlab.setup{}
 require'lspconfig'.clangd.setup{}
-require'lspconfig'.pylsp.setup{}
--- vim.lsp.handlers["textDocument/publishDiagnostics"] = true
--- vim.lsp.handlers["textDocument/publishDiagnostics"] = false
--- vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
---   vim.lsp.diagnostic.on_publish_diagnostics, {
---     -- Enable underline, use default values
---     underline = true,
---     -- Enable virtual text, override spacing to 4
---     virtual_text = true,
---     signs = {
---       enable = true,
---       priority = 20
---     },
---     -- Disable a feature
---     update_in_insert = false,
--- })
+-- require'lspconfig'.pylsp.setup{}
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.vimls.setup{}
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] =vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics,
     {
-        update_in_insert =true,
+        update_in_insert =false,
         virtual_text = {
             prefix = "➤"
-            -- "➤"
         },
         signs = {
-                enable = true,
-                priority = 20
+              enable = true,
+              priority = 20
             },
     }
     )
-vim.fn.sign_define(
-    "LspDiagnosticsSignError",
-    {
-        text = "✗",
-        -- texthl = "LspDiagnosticsSignError"
-    }
-    )
-vim.fn.sign_define(
-    "LspDiagnosticsSignWarning",
-    {
-        text = "⚡",
-        -- texthl = "LspDiagnosticsSignWarning"
-    }
-    )
+-- local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "✗", Warn = "⚡", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+-- vim.fn.sign_define(
+--     "LspDiagnosticsSignError",
+--     {
+--         text = "✗",
+--         -- texthl = "LspDiagnosticsSignError"
+--     }
+--     )
+-- vim.fn.sign_define(
+--     "LspDiagnosticsSignWarning",
+--     {
+--         text = "⚡",
+--         -- texthl = "LspDiagnosticsSignWarning"
+--     }
+--     )
 local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
     buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-
-
-    -- Mappings.
-    local opts = { noremap=true, silent=true }
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<space>lk', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', '<space>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap('n', '<space>lr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>lt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>le', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-    buf_set_keymap('n', '<space>ld', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-
-    -- Set some keybinds conditional on server capabilities
-    --if client.resolved_capabilities.document_formatting then
-    --buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    --elseif client.resolved_capabilities.document_range_formatting then
-    --buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-    --end
 
     -- Set autocommands conditional on server_capabilities
     if client.resolved_capabilities.document_highlight then
@@ -95,10 +60,87 @@ local on_attach = function(client, bufnr)
     end
 end
 
+-- lsp  Go-to definition in a split window
+local function goto_definition(split_cmd)
+  local util = vim.lsp.util
+  local log = require("vim.lsp.log")
+  local api = vim.api
+
+  -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
+  local handler = function(_, result, ctx)
+    if result == nil or vim.tbl_isempty(result) then
+      local _ = log.info() and log.info(ctx.method, "No location found")
+      return nil
+    end
+
+    if split_cmd then
+      vim.cmd(split_cmd)
+    end
+
+    if vim.tbl_islist(result) then
+      util.jump_to_location(result[1])
+
+      if #result > 1 then
+        util.set_qflist(util.locations_to_items(result))
+        api.nvim_command("copen")
+        api.nvim_command("wincmd p")
+      end
+    else
+      util.jump_to_location(result)
+    end
+  end
+
+  return handler
+end
+
+vim.lsp.handlers["textDocument/definition"] = goto_definition('split')
+
+
+
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = { "clangd", "texlab", "pylsp","vimls" }
+local servers = { "clangd", "texlab", "pyright","vimls" }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup { on_attach = on_attach }
 end
 
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown', 'plaintext' }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+    properties = {
+        'documentation',
+        'detail',
+        'additionalTextEdits',
+    },
+}
+Itkey_capabilities = capabilities
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = {'tsserver'}
+for _, lsp in ipairs(servers) do
+    nvim_lsp[lsp].setup {
+        on_attach = Itkey_on_attach,
+        capabilities = Itkey_capabilities,
+    }
+end
+
+local saga = require 'lspsaga'
+
+saga.init_lsp_saga {
+
+-- add your config value here
+-- default value
+use_saga_diagnostic_sign = true,
+    error_sign = '✗',
+    warn_sign = '⚡',
+    hint_sign = '●',
+    infor_sign = '●',
+
+}
